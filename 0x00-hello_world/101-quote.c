138211732e6b7b88684ac328ddcde66758271e63
ftyelbi <ftyelbi@gmail.com>
2023-06-15 17:20:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - This C program prints a line to the standard error
- * Return 1 (Successful)
+ * main - Entry point of the program
+ *
+ * Description: This C program prints a line to the standard error stream
+ *
+ * Return: 1 (Success)
  */
 int main(void)
 {
 fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
-}
+
