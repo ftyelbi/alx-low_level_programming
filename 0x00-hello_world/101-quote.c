f3c86c9599b4f5062bae703034befa0447d0cc0a
ftyelbi <ftyelbi@gmail.com>
2023-06-15 17:16:07 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - This C program prints a line to the standard error
+ * Return 1 (Successful)
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
