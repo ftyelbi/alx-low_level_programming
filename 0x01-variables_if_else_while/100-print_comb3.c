b0cee0520a7b884dc2b1e46965fefc2ec674f4ad
ftyelbi <ftyelbi@gmail.com>
2023-06-16 17:54:24 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  *main - Entry Point
+  *Description: 'This prints possible different comb. of two digits'
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+int n, m;
+	for (n = '0'; n < '9' ; n++)
+	{
+		for (m = n + 1; m <= '9' ; m++)
+		{
+			if (m != n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n == '8' && m == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
