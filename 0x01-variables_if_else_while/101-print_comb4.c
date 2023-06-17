b14d894dccb6d2530ef15505eb6e10a9e5ff54b3
ftyelbi <ftyelbi@gmail.com>
2023-06-16 18:06:20 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  *main - Entry Point
+  *Description: 'This prints all possible comb. of three digits'
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+int n, m, k;
+	for (n = '0' ; n < '9' ; n++)
+	{
+		for (m = n + 1; m <= '9' ; m++)
+		{
+			for (k = m + 1; k <= '9' ; k++)
+			{
+				if ((m != n) != k)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(k);
+					if (n == '7' && m == '8')
+						continue;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
